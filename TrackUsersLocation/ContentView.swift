cd63105f3e02e76cda817266bfda81ca67e66140
Habip Yeşilyurt <yesilyurthabip@hotmail.com>
2024-04-05 18:46:47 +0300

    Initial Commit

---
+++ TrackUsersLocation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrackUsersLocation
+//
+//  Created by Habip Yesilyurt on 5.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrackUsersLocation/TrackUsersLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackUsersLocationApp.swift
+//  TrackUsersLocation
+//
+//  Created by Habip Yesilyurt on 5.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrackUsersLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
